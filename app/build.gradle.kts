551866eff3f077bb26eaa0c8f7fa89262f5ba008
Ignacio Duran <ignaciod54332@gmail.com>
2024-11-22 11:10:09 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.cameraX.core)
     implementation(libs.cameraX.camera2)
     implementation(libs.cameraX.lifecicle)
+    implementation(libs.cameraX.view)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
