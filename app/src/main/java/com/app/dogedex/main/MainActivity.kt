60b2965455429b2b216fa950a3a1a724c32f5e38
Ignacio Duran <ignaciod54332@gmail.com>
2024-12-07 14:02:35 -0600

    Update MainActivity.kt

--- app/src/main/java/com/app/dogedex/main/MainActivity.kt
+++ app/src/main/java/com/app/dogedex/main/MainActivity.kt
@@ -66,7 +66,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        requestedOrientation.
         val user = User.getLoggedInUser(this)
         if (user == null) {
             openLoginActivity()
